014114ddf31ec764d7f8732cc2506160d367b28d
incapaz <60857303+zapacni@users.noreply.github.com>
2021-01-03 16:25:38 -0800

    Add type annotations

--- main.lua
+++ main.lua
@@ -2,81 +2,45 @@
 
 local Convert = {}
 
-function Convert.UDim2ToVector2(udim2)
+function Convert.UDim2ToVector2(udim2: UDim2): Vector2
 	return Vector2.new(udim2.X.Offset, udim2.Y.Offset)
 end
 
-function Convert.Vector2ToUDim2(vector2)
+function Convert.Vector2ToUDim2(vector2: Vector2): UDim2
 	return UDim2.fromOffset(vector2.X, vector2.Y)
 end
 
-function Convert.StringToBool(str)
-	if str == "true" or str == "false" then
-		local boolNew
-		
-		if str == "true" then
-			boolNew = true
-		elseif str == "false" then
-			boolNew = false
-		end
-		
-		return boolNew
-	else
-		warn("Parameter must be a string!")
-		return nil
-	end
+function Convert.StringToBool(str: string): boolean
+	local string_booleans = { "true", "false" }
+	return table.remove(string_booleans, table.find(string_booleans, str)) or error("not boolean repr")
 end
 
-function Convert.BoolToString(bool)
-	if bool == true or bool == false then
-		local stringNew
-		
-		if bool == true then
-			stringNew = "true"
-		elseif bool == false then
-			stringNew = "false"
-		end
-		
-		return stringNew
-	else
-		warn("Parameter must be a boolean!")
-		return "nil"
-	end
+function Convert.BoolToString(bool: boolean): string
+	return tostring(bool)
 end
 
-function Convert.Color3ToBrickColor(color3)
+function Convert.Color3ToBrickColor(color3: Color3): BrickColor
 	return BrickColor.new(color3)
 end
 
-function Convert.BrickColorToColor3(brickcolor)
-	return brickcolor.color
+function Convert.BrickColorToColor3(brickcolor: BrickColor): Color3
+	return brickcolor.Color
 end
 
-function Convert.Vector3int16ToVector3(vector16)
+function Convert.Vector3int16ToVector3(vector16: Vector3int16): Vector3
 	return Vector3.new(vector16.X, vector16.Y, vector16.Z)
 end
 
-function Convert.Vector3ToVector3int16(vector3)
+function Convert.Vector3ToVector3int16(vector3: Vector3): Vector3int16
 	return Vector3int16.new(vector3.X, vector3.Y, vector3.Z)
 end
 
-function Convert.Vector3ToCFrame(vector3)
-	if (not vector3.X) or (not vector3.Y) or (not vector3.Z) then
-		warn("Parameter must be a Vector3!")
-		return
-	else
-		return CFrame.new(vector3)
-	end
+function Convert.Vector3ToCFrame(vector3: Vector3): CFrame
+	return CFrame.new(vector3)
 end
 
-function Convert.CFrameToVector3(cframe)
-	if (not cframe.X) or (not cframe.Y) or (not cframe.Z) then
-		warn("Parameter must be a CFrame!")
-		return
-	else
-		return Vector3.new(cframe.X, cframe.Y, cframe.Z)
-	end
+function Convert.CFrameToVector3(cframe: CFrame): Vector3
+	return Vector3.new(cframe.X, cframe.Y, cframe.Z)
 end
 
-return Convert
 return Convert
